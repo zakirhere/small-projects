bfce61bc9882462bb37010acc5b0ab2bd8ecbe29
Zakir Sayed <zakirhere@gmail.com>
2014-12-27 11:06:42 -0500

    ball project added

---
+++ baml_otp/src/remoteLogin/baml_vpn.java
@@ -0,0 +1,17 @@
+package remoteLogin;
+
+public class baml_vpn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void launchVPN() {
+		
+	}
+	
+	public void obtainVirtualToken() {
+		
+	}
+}
---
+++ baml_otp/src/remoteLogin/otp_details.java
@@ -0,0 +1,15 @@
+package remoteLogin;
+
+public class otp_details {
+	public void launchOTPpage() {
+		
+	}
+	
+	public void enterFormOTP() {
+		
+	}
+	
+	public void clickLogon() {
+		
+	}
+}
